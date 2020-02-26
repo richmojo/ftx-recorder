257a87cc5a189f6b28617f9ea87396b9ae3c62bc
Josh <jrichmond2009@gmail.com>
2020-02-25 21:08:21 -0500

    added metrics

--- account_recorder.py
+++ account_recorder.py
@@ -44,53 +44,79 @@ def run(sub):
             logger.error(f"Could not get account with error: {e}")
             raise e
         else:
-            logger.info("Writing account.")
             t = datetime.utcnow().isoformat()
             account = account["result"]
             positions = account["positions"]
 
             account_write = {
                 "measurement": "account",
-                "tags": {
-                    "username": account["username"],
-                },
+                "tags": {"username": account["username"],},
                 "fields": {
                     "collateral": account["collateral"],
                     "freeCollateral": account["freeCollateral"],
+                    "percentUsedCollateral": (
+                        account["collateral"] - account["freeCollateral"]
+                    )
+                    / account["collateral"],
+                    "percentFreeCollateral": account["freeCollateral"]
+                    / account["collateral"],
                     "marginFraction": account["marginFraction"],
                     "openMarginFraction": account["openMarginFraction"],
                     "totalAccountValue": account["totalAccountValue"],
                     "totalPositionSize": account["totalPositionSize"],
+                    "currentLeverage": account["totalPositionSize"] / account["collateral"],
                 },
                 "time": t,
             }
-            account_write["fields"] = {k: float(v) for k, v in account_write["fields"].items() if v is not None}
+            account_write["fields"] = {
+                k: float(v) for k, v in account_write["fields"].items() if v is not None
+            }
             client.write_points([account_write])
 
             if positions:
-                logger.info("Writing positions.")
-                positions_write = [{
-                    "measurement": "positions",
-                    "tags": {
-                        "future": p["future"],
-                        "side": p["side"],
-                    },
+                positions_write = [
+                    {
+                        "measurement": "positions",
+                        "tags": {"future": p["future"], "side": p["side"],},
+                        "fields": {
+                            "collateralUsed": p["collateralUsed"],
+                            "cost": p["cost"],
+                            "entryPrice": p["entryPrice"],
+                            "estimatedLiquidationPrice": p["estimatedLiquidationPrice"],
+                            "netSize": p["netSize"],
+                            "openSize": p["openSize"],
+                            "realizedPnl": p["realizedPnl"],
+                            "size": p["size"],
+                            "unrealizedPnl": p["unrealizedPnl"],
+                        },
+                        "time": t,
+                    }
+                    for p in positions
+                ]
+                for p in positions_write:
+                    p["fields"] = {
+                        k: float(v) for k, v in p["fields"].items() if v is not None
+                    }
+                client.write_points(positions_write)
+
+                all_positions_write = {
+                    "measurement": "all_positions",
                     "fields": {
-                        "collateralUsed": p["collateralUsed"],
-                        "cost": p["cost"],
-                        "entryPrice": p["entryPrice"],
-                        "estimatedLiquidationPrice": p["estimatedLiquidationPrice"],
-                        "netSize": p["netSize"],
-                        "openSize": p["openSize"],
-                        "realizedPnl": p["realizedPnl"],
-                        "size": p["size"],
-                        "unrealizedPnl": p["unrealizedPnl"],
+                        "totalCollateralUsed": sum(
+                            [p["collateralUsed"] for p in positions]
+                        ),
+                        "totalCost": sum([p["cost"] for p in positions]),
+                        "totalRealizedPnl": sum([p["realizedPnl"] for p in positions]),
+                        "totalUnrealizedPnl": sum([p["unrealizedPnl"] for p in positions]),
                     },
                     "time": t,
-                } for p in positions]
-                for p in positions_write:
-                    p["fields"] = {k: float(v) for k, v in p["fields"].items() if v is not None}
-                client.write_points(positions_write)
+                }
+                all_positions_write["fields"] = {
+                    k: float(v)
+                    for k, v in all_positions_write["fields"].items()
+                    if v is not None
+                }
+                client.write_points([all_positions_write])
 
 
     def get_balances(client):
@@ -100,22 +126,22 @@ def run(sub):
             logger.error(f"Could not get balances with error: {e}")
             raise e
         else:
-            logger.info("Writing balances.")
             t = datetime.utcnow().isoformat()
             balances = balances["info"]["result"]
 
-            balances_write = [{
-                "measurement": "balances",
-                "tags": {
-                    "coin": c["coin"],
-                },
-                "fields": {
-                    "free": float(c["free"]),
-                    "total": float(c["total"]),
-                    "usdValue": float(c["usdValue"]),
-                },
-                "time": t,
-            } for c in balances]
+            balances_write = [
+                {
+                    "measurement": "balances",
+                    "tags": {"coin": c["coin"],},
+                    "fields": {
+                        "free": float(c["free"]),
+                        "total": float(c["total"]),
+                        "usdValue": float(c["usdValue"]),
+                    },
+                    "time": t,
+                }
+                for c in balances
+            ]
             client.write_points(balances_write)
 
 
@@ -126,37 +152,41 @@ def run(sub):
             since = int(time.time() - 120)
 
         try:
-            orders = Exchange.privateGetOrdersHistory(params={'start_time': since})
+            orders = Exchange.privateGetOrdersHistory(params={"start_time": since})
         except ccxt.BaseError as e:
             logger.error(f"Could not get order history with error: {e}")
             raise e
         else:
-            logger.info("Writing orders.")
             orders = orders["result"]
 
             if orders:
-                orders_write = [{
-                    "measurement": "orders",
-                    "tags": {
-                        "future": o["future"],
-                        "market": o["market"],
-                        "type": o["type"],
-                        "side": o["side"],
-                        "reduceOnly": o["reduceOnly"],
-                        "status": o["status"],
-                        "postOnly": o["postOnly"],
-                    },
-                    "fields": {
-                        "avgFillPrice": o["avgFillPrice"],
-                        "filledSize": o["filledSize"],
-                        "price": o["price"],
-                        "size": o["size"],
-                    },
-                    "time": o["createdAt"][:-6] + 'Z',
-                } for o in orders]
+                orders_write = [
+                    {
+                        "measurement": "orders",
+                        "tags": {
+                            "future": o["future"],
+                            "market": o["market"],
+                            "type": o["type"],
+                            "side": o["side"],
+                            "reduceOnly": o["reduceOnly"],
+                            "status": o["status"],
+                            "postOnly": o["postOnly"],
+                        },
+                        "fields": {
+                            "avgFillPrice": o["avgFillPrice"],
+                            "filledSize": o["filledSize"],
+                            "price": o["price"],
+                            "size": o["size"],
+                        },
+                        "time": o["createdAt"][:-6] + "Z",
+                    }
+                    for o in orders
+                ]
                 for o in orders_write:
                     o["tags"] = {k: str(v) for k, v in o["tags"].items() if v is not None}
-                    o["fields"] = {k: float(v) for k, v in o["fields"].items() if v is not None}
+                    o["fields"] = {
+                        k: float(v) for k, v in o["fields"].items() if v is not None
+                    }
                 client.write_points(orders_write)
 
 
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ MainConfig = {
         "test": False,
         "api_key": "MpSTlxy_BxSsEbKpx4DEbXGHqwBvjKdx1m2w9Uig",
         "api_secret": "W6ZVy9cQMMAL_0z8h8H1jCPIj_gBdfJO2AhHWI-O",
-        "subaccount": ("BTC", "ETH", "EOS", "LINK", "XTZ", "BCH", "LTC", "ALGO", "TOMO"),
+        "subaccount": ("BTC", "ETH", "EOS", "LINK", "XTZ", "BCH", "LTC", "ALGO", "TOMO", "OKB"),
     },
 }
 
