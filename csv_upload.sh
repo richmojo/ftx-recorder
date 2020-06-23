dc0552601643bc680bd1759db9432a22f0c8f7fb
Josh <jrichmond2009@gmail.com>
2020-06-22 22:13:27 -0400

    sh

--- csv_upload.sh
+++ csv_upload.sh
@@ -1,12 +1,13 @@
 # get csv's
-
+echo "Getting csv's"
 influx -database 'BTC_accountdb' -execute 'select * from account' -format 'csv' > btc_account.csv
 
 # move csv's
-
+echo "Moving csv's"
 mv btc_account.csv Dropbox-Uploader/btc_account.csv
 
 # upload csv's
-
+echo "Uploading csv's"
+cd ..
 cd Dropbox-Uploader
 ./dropbox_uploader.sh upload btc_account.csv /dropbox/rltrader
