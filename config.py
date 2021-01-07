aa6b318e407db4bd65084409982aee9a60f50d48
Josh Richmond <jrichmond2009@gmail.com>
2021-01-07 16:25:42 -0500

    switched recorder

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ MainConfig = {
     "Exchange": {
         "exchange": "ftx",  # "bitmex" "ftx"
         "test": False,
-        "api_key": "MpSTlxy_BxSsEbKpx4DEbXGHqwBvjKdx1m2w9Uig",
-        "api_secret": "W6ZVy9cQMMAL_0z8h8H1jCPIj_gBdfJO2AhHWI-O",
+        "api_key": "yxcSkVATXUgiChKinwhd9-jTZLNbz3Bu-C986RZI",
+        "api_secret": "IoGp5vmE001BGaSYLJNViIXpw12qVkbEkUeyVUrW",
     },
 }
 
