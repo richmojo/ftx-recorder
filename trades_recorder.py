6a867e9892d31f7bef76399801f6df4d21f7ea58
Josh Richmond <jrichmond2009@gmail.com>
2021-02-21 17:27:57 -0500

    fix oi and book

--- trades_recorder.py
+++ trades_recorder.py
@@ -1,4 +1,4 @@
-from cryptofeed.backends.influxdb import TradeInflux
+from cryptofeed.backends.influxdb import TradeInflux,  BookInflux, OpenInterestInflux
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import FTX
 from cryptofeed.defines import TRADES
@@ -23,8 +23,8 @@ PAIRS = [pair for pair in PAIRS if pair.endswith("PERP")]
 def main():
     f = FeedHandler()
     trade_influx = TradeInflux('http://localhost:8086', 'tradesdb', create_db=True, numeric_type=float)
-    orderbook_influx = TradeInflux('http://localhost:8086', 'orderbookdb', create_db=True, numeric_type=float)
-    oi_influx = TradeInflux('http://localhost:8086', 'oidb', create_db=True, numeric_type=float)
+    orderbook_influx = BookInflux('http://localhost:8086', 'orderbookdb', create_db=True, numeric_type=float)
+    oi_influx = OpenInterestInflux('http://localhost:8086', 'oidb', create_db=True, numeric_type=float)
     f.add_feed(FTX(channels=[TRADES], pairs=PAIRS, callbacks={TRADES: trade_influx}), timeout=30)
     f.add_feed(FTX(channels=[OPEN_INTEREST], pairs=PAIRS, callbacks={OPEN_INTEREST: oi_influx}), timeout=30)
     f.add_feed(FTX(channels=[L2_BOOK], pairs=['BTC-PERP', 'ETH-PERP'], callbacks={L2_BOOK: orderbook_influx}), timeout=30)
