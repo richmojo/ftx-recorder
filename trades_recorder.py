04ea58597424457840e6c39a2e3def80fd3a5866
Josh Richmond <jrichmond2009@gmail.com>
2021-02-21 17:40:56 -0500

    removed orderbook

--- trades_recorder.py
+++ trades_recorder.py
@@ -23,11 +23,11 @@ PAIRS = [pair for pair in PAIRS if pair.endswith("PERP")]
 def main():
     f = FeedHandler()
     trade_influx = TradeInflux('http://localhost:8086', 'tradesdb', create_db=True, numeric_type=float)
-    orderbook_influx = BookInflux('http://localhost:8086', 'orderbookdb', create_db=True, numeric_type=float)
+    #orderbook_influx = BookInflux('http://localhost:8086', 'orderbookdb', create_db=True, numeric_type=float)
     oi_influx = OpenInterestInflux('http://localhost:8086', 'oidb', create_db=True, numeric_type=float)
     f.add_feed(FTX(channels=[TRADES], pairs=PAIRS, callbacks={TRADES: trade_influx}), timeout=30)
     f.add_feed(FTX(channels=[OPEN_INTEREST], pairs=PAIRS, callbacks={OPEN_INTEREST: oi_influx}), timeout=30)
-    f.add_feed(FTX(channels=[L2_BOOK], pairs=['BTC-PERP', 'ETH-PERP'], callbacks={L2_BOOK: orderbook_influx}), timeout=30)
+    #f.add_feed(FTX(channels=[L2_BOOK], pairs=['BTC-PERP', 'ETH-PERP'], callbacks={L2_BOOK: orderbook_influx}), timeout=30)
     f.run()
 
 
